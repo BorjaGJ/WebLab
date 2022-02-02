076d5cd4bbbd45bafadee35a07155162d48ca151
BorjaGJ <borjagj25@gmail.com>
2022-02-02 13:20:24 +0100

    editar, eliminar y añadir reactivos

--- productos/urls.py
+++ productos/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # url(r'^reactivos/(?P<codigo_laboratorio>[-\w]+)$', login_required(views.ReactivosDetailView.as_view()), name='reactivo_detalle'),
     url(r'reactivos/add$', login_required(views.AddReactivo.as_view()), name='add_reactivo'),
     url(r'^reactivos/editar/(?P<codigo_laboratorio>[-\w]+)$', login_required(views.ReactivosEditView.as_view()), name='reactivo_edit'),
-    url(r'/delete/(?P<codigo_laboratorio>[-\w]+)$', login_required(views.DeleteReactivo), name='delete_reactivo'),
+    url(r'^delete/(?P<codigo_laboratorio>[-\w]+)$', login_required(views.DeleteReactivo), name='delete_reactivo'),
     url(r'^disolventes$', login_required(views.DisolventesView.as_view()), name='disolventes'),
     url(r'^patrones$', login_required(views.PatronesView.as_view()), name='patrones'),
 
