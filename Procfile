092e160c54c06ab34dd3483b542407c537badee6
BorjaGJ <borjagj25@gmail.com>
2022-04-12 18:18:43 +0200

    instalacion heroku me rendi

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn WebLab.wsgi
--- configuracion/settings.py
+++ configuracion/settings.py
@@ -143,17 +143,13 @@ LOCALE_PATHS = (
 
 DATABASES = local_settings.DATABASES
 
-"""
-para poder instalar con apache hay que descomentar static root y comentar
-los staticfiles_dirs hacer python manage.py collectstatic y dejar comoe staba
-"""
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # os.path.join(BASE_DIR, "static"),
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -200,7 +196,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 MEDIA_URL = '/media/'
