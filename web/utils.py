b878270c4f4ef45e6be524d2465ecfc8a8b62389
BorjaGJ <borjagj25@gmail.com>
2022-04-24 22:42:21 +0200

    calendario blanco

--- web/utils.py
+++ web/utils.py
@@ -43,7 +43,7 @@ class MyHTMLCalendar(HTMLCalendar):
 
         else:
              # cualquier otro dia
-            return '<td class="%s">%d</td>' % ('calendar-day', day)
+            return '<td class="%s">%d<br><strong class="text-white">0 Eventos</strong></td>' % ('calendar-day', day)
 
     def formatweekday(self, day):
         """
