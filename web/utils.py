4f5267de4c0bca166661854029ee29a38fd3f853
BorjaGJ <borjagj25@gmail.com>
2022-05-02 21:03:23 +0200

    matar luciernagas

--- web/utils.py
+++ web/utils.py
@@ -32,7 +32,7 @@ class MyHTMLCalendar(HTMLCalendar):
             return '<td class="calendar-day" style="background-color:%s;' \
                    'background-image:linear-gradient(to right, %s)">' \
                    '<a class="text-black text-decoration-none"></a>%d <br>'  \
-                   '<a class="font-weight-bold text-decoration-none glow" ' \
+                   '<a class="font-weight-bold text-decoration-none" ' \
                    'href="calendario/eventos/%d/%d" class="active">%s %s </a>' \
                    '</td>' \
                    % \
